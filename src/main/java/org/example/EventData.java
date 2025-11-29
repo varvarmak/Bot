e5154d7a26c6027fb1bcab90f8dc2a7f5e7a665e
varvara <sofam2135@gmail.com>
2025-11-29 19:36:48 +0500

    мама мама мама

--- src/main/java/org/example/EventData.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class EventData {
-    public int year;
-    public int month;
-    public int day;
-    public String time;
-    public String title;
-}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
-
 package org.example;
 
+import org.example.bot.TelegramBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
